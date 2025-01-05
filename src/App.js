f7c4e7dade3205efdd0c18e0dc8028ac59a218b8
Donggun <donggunl@usc.edu>
2025-01-04 22:03:09 -0800

    Header initial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import kseaLogo from './assets/ksea-logo.avif';
+import kseaBanner from './assets/ksea-banner.avif';
 
 function App() {
   return (
     <div className="App">
+      <div className="heading">
+      <img src={kseaBanner} alt="KSEA Banner" className="banner"/>
+      <p className="title">Korean-American Scientists and Engineers Association at USC</p>
+      <img src={kseaLogo} alt="KSEA Logo" className="logo"/>
+      </div>
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
