34e6329302dcafe47e4758b644641cc21d9d5f42
Donggun <donggunl@usc.edu>
2025-02-27 19:34:30 -0800

    Added tailwind

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,25 +3,29 @@
 const Navbar = () => {
 
     return (
-        <nav className="navbar">
-            <li>
-                <a href="/" className="nav-link">Home</a>
-            </li>
-            <li>
-                <a href="/" className="nav-link">About</a>
-            </li>
-            <li>
-                <a href="/" className="nav-link">Our Team</a>
-            </li>
-            <li>
-                <a href="/" className="nav-link">How to Join</a>
-            </li>
-            <li>
-                <a href="/" className="nav-link">Gallery</a>
-            </li>
-            <li>
-                <a href="/" className="nav-link">Contact Us</a>
-            </li>
+        <nav className="w-full p-4">
+            <ul className="flex justify-center space-x-6">
+                <li>
+                    <a href="/" className="nav-link">Home</a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link">About</a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link">Our Team</a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link">How to Join</a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link">Gallery</a>
+                </li>
+                <li>
+                    <a href="/" className="nav-link">Contact Us</a>
+                </li>
+
+            </ul>
+
         </nav>
     );
 
