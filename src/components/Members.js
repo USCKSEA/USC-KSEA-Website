099aa99739c138e6f36cfea7697b97a5c75df4af
Donggun <donggunl@usc.edu>
2025-04-13 17:02:19 -0700

    Teams update

--- src/components/Members.js
+++ src/components/Members.js
@@ -6,18 +6,18 @@ import junsoo from '../assets/team/junsoo.JPG';
 import jae from '../assets/team/jae.JPG';
 import jacob from '../assets/team/jacob.JPG';
 import adam from '../assets/team/adam.jpeg';
-// import esther from '../assets/team/esther.jpeg';
+import esther from '../assets/team/esther.JPG';
 // import kate from '../assets/team/kate.jpeg';
-// import donggun from '../assets/team/donggun.jpeg';
+import donggun from '../assets/team/donggun.JPG';
 // import parker from '../assets/team/parker.jpeg';
 // import ryan from '../assets/team/ryan.jpeg';
-// import charlotte from '../assets/team/charlotte.jpeg';
+import charlotte from '../assets/team/charlotte.jpeg';
 // import kyungtae from '../assets/team/kyungtae.jpeg';
-// import suna from '../assets/team/suna.jpeg';
-// import hoyeol from '../assets/team/hoyeol.jpeg';
+import suna from '../assets/team/suna.JPG';
+import hoyeol from '../assets/team/hoyeol.jpeg';
 // import erica from '../assets/team/erica.jpeg';
 // import phillip from '../assets/team/phillip.jpeg';
-// import kaiden from '../assets/team/kaiden.jpeg';
+import kaiden from '../assets/team/kaiden.JPG';
 // import andrew from '../assets/team/andrew.jpeg';
 
     const Members = [
@@ -51,24 +51,24 @@ import adam from '../assets/team/adam.jpeg';
             role: 'Finance Lead',
             description: 'asdf',
         },
-        // {
-        //     image: esther,
-        //     name: 'Esther Chung',
-        //     role: 'Events Lead',
-        //     description: '',
-        // },
+        {
+            image: esther,
+            name: 'Esther Chung',
+            role: 'Events Lead',
+            description: '',
+        },
         // {
         //     image: kate,
         //     name: 'Kate Shin',
         //     role: 'Design Chair',
         //     description: '',
         // },
-        // {
-        //     image: donggun,
-        //     name: 'Donggun Lee',
-        //     role: 'Proj Dev Lead',
-        //     description: '',
-        // },
+        {
+            image: donggun,
+            name: 'Donggun Lee',
+            role: 'Proj Dev Lead',
+            description: '',
+        },
         // {
         //     image: parker,
         //     name: 'Parker Chang',
@@ -93,30 +93,30 @@ import adam from '../assets/team/adam.jpeg';
         //     role: 'Finance',
         //     description: '',
         // },
-        // {
-        //     image: charlotte,
-        //     name: 'Charlotte Kwon',
-        //     role: 'Events',
-        //     description: '',
-        // },
+        {
+            image: charlotte,
+            name: 'Charlotte Kwon',
+            role: 'Events',
+            description: '',
+        },
         // {
         //     image: kyungtae,
         //     name: 'Kyungtae Kim',
         //     role: 'Events',
         //     description: '',
         // },
-        // {
-        //     image: suna,
-        //     name: 'Sun-A Choe',
-        //     role: 'Proj Dev',
-        //     description: '',
-        // },
-        // {
-        //     image: hoyeol,
-        //     name: 'Hoyeol Baek',
-        //     role: '',
-        //     description: '',
-        // },
+        {
+            image: suna,
+            name: 'Sun-A Choe',
+            role: 'Proj Dev',
+            description: '',
+        },
+        {
+            image: hoyeol,
+            name: 'Hoyeol Baek',
+            role: 'Proj Dev',
+            description: '',
+        },
         {
             image: juyeop,
             name: 'Juyeop Lee',
@@ -135,12 +135,12 @@ import adam from '../assets/team/adam.jpeg';
         //     role: 'PR/Marketing',
         //     description: '',
         // },
-        // {
-        //     image: kaiden,
-        //     name: 'Kaiden Ko',
-        //     role: 'Freshmen Rep',
-        //     description: '',
-        // },
+        {
+            image: kaiden,
+            name: 'Kaiden Ko',
+            role: 'Freshmen Rep',
+            description: '',
+        },
         // {
         //     image: andrew,
         //     name: 'Andrew Park',
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,9 +1,33 @@
 import PageContent from "../components/PageContent";
 import Members from "../components/Members";
 import React from 'react';
+import image1 from "../assets/team/banquet.JPG";
+import image2 from "../assets/team/group.JPG";
+import image3 from "../assets/team/skitrip.jpeg";
+import { useEffect, useState } from 'react';
 
+
+const images = [image1, image2, image3];
 
 const Team = () => {
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(prev =>
+                prev === images.length - 1 ? 0 : prev + 1
+            );
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+
+    const prevSlide = () => 
+        setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+    const nextSlide = () => 
+        setCurrentIndex((currentIndex + 1) % images.length);
     return (
         <PageContent>
         <div className="flex flex-col gap-4 mx-auto w-[60vw] max-w-[800px] sm:w-[60vw] md:w-[70vw] mb-10">
@@ -11,9 +35,27 @@ const Team = () => {
                 <h1 className="font-bold text-5xl m-[5vh]">Meet Our Team</h1>
             </div>
             
-            <div className="h-[50vh] w-90 items-center justify-center border-2">
-                Some image goes here
-            </div>
+            {/* Image Slide h-[50vh]*/}
+    <div className="relative h-[50vh] w-full border-2 overflow-hidden">
+    {images.map((img, index) => (
+        <img
+        key={index}
+        src={img}
+        alt={`Slide ${index}`}
+        className={`absolute top-0 left-0 w-full h-full object-cover object-center transition-opacity duration-1000 
+            ${index === currentIndex ? 'opacity-100 z-10' : 'opacity-0 z-0'}
+        `}
+        />
+    ))}
+
+    <button onClick={prevSlide} className="absolute left-0 top-1/2 -translate-y-1/2 px-3 py-1 bg-gray-700 text-white z-20">
+        ‹
+    </button>
+    
+    <button onClick={nextSlide} className="absolute right-0 top-1/2 -translate-y-1/2 px-3 py-1 bg-gray-700 text-white z-20">
+        ›
+    </button>
+    </div>
 
             <p>The <span className="font-bold">Executive Board ("E-Board")</span> functions as the governing body of the USC KSEA YG Group. Each Spring Semester, the three new presidents (two Co-Presidents and one Vice President) are selected to lead the E-Board for the following school year. The presidents lead the E-Board through internal management and represent the group through external communications with our community.</p>
 
@@ -32,6 +74,7 @@ const Team = () => {
             </p>
 
 
+            {/* Team members */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 w-full gap-5 mt-[8vh]">
                 {Members.map((item, index) => {
                     return (
