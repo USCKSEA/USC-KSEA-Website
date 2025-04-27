623f2ef17ea01a3132ea6fe6830b739785aa8ac8
Donggun <ldgbilly@gmail.com>
2025-04-27 06:14:27 -0700

    Testing new login

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -19,6 +19,7 @@ const Gallery = () => {
                 <div className="w-full border-2">asdf</div>
                 <div className="w-full border-2">asdf</div>
                 <div className="w-full border-2">asdf</div>
+                <div className="w-full border-2">asdf</div>
                 <div className="flex flex-col w-full border-2 justfy-center items-center">
                     <div><img src={event1} alt="KSEA Logo" className="w-full mb-5" /></div>
                     <p className="m-5">Fundraiser</p>
