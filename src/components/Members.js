4637d088d382a9b7b41298118417f769fc303dfb
Sun A Choe <sunachoe@usc.edu>
2025-08-13 16:25:59 +0900

    updated roles

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,20 +1,20 @@
-import sangwon from '../assets/team/sangwon.JPG'
+// import sangwon from '../assets/team/sangwon.JPG'
 import noah from '../assets/team/noah.JPG';
-import michael from '../assets/team/michael.JPG';
+// import michael from '../assets/team/michael.JPG';
 import juyeop from '../assets/team/juyeop.JPG';
 import junsoo from '../assets/team/junsoo.JPG';
-import jae from '../assets/team/jae.JPG';
+// import jae from '../assets/team/jae.JPG';
 import jacob from '../assets/team/jacob.JPG';
 import adam from '../assets/team/adam.jpeg';
 import esther from '../assets/team/esther.JPG';
 // import kate from '../assets/team/kate.jpeg';
-import donggun from '../assets/team/donggun.JPG';
+// import donggun from '../assets/team/donggun.JPG';
 // import parker from '../assets/team/parker.jpeg';
 // import ryan from '../assets/team/ryan.jpeg';
 import charlotte from '../assets/team/charlotte.jpeg';
 // import kyungtae from '../assets/team/kyungtae.jpeg';
 import suna from '../assets/team/suna.JPG';
-import hoyeol from '../assets/team/hoyeol.jpeg';
+// import hoyeol from '../assets/team/hoyeol.jpeg';
 // import erica from '../assets/team/erica.jpeg';
 // import phillip from '../assets/team/phillip.jpeg';
 import kaiden from '../assets/team/kaiden.JPG';
@@ -22,71 +22,77 @@ import kaiden from '../assets/team/kaiden.JPG';
 
     const Members = [
         {
-            image: sangwon,
-            name:  'Sangwon Cha',
+            image: adam,
+            name:  'Adam Kim',
             role:  'President',
-            description: 'Lorem Ipsum',
+            description: 'Intern @ Coca-Cola Bottling',
+        },
+        {
+            image: junsoo,
+            name: 'Junsoo Kim',
+            role: 'Vice President',
+            description: 'asdf',
         },
         {
             image: noah,
             name: 'Noah Kim',
-            role: 'Secretary',
-            description: 'Lorem Ipsum',
-        },
-        {
-            image: jae,
-            name: 'Jae Cho',
             role: 'Senior Advisor',
-            description: 'Danford',
-        },
-        {
-            image: michael,
-            name: 'Michael Kim',
-            role: 'Events Lead',
-            description: 'asdf',
-        },
-        {
-            image: adam,
-            name: 'Adam Kim',
-            role: 'Finance Lead',
-            description: 'asdf',
+            description: 'Lorem Ipsum',
         },
         {
             image: esther,
             name: 'Esther Chung',
-            role: 'Events Lead',
+            role: 'Senior Advisor',
+            description: 'Esther',
+        },
+        {
+            image: jacob,
+            name: 'Jacob Cho',
+            role: 'Finance Lead',
+            description: 'asdf',
+        },
+        {
+            image: juyeop,
+            name: 'Juyeop Lee',
+            role: 'Professional Events Lead',
+            description: '',
+        },
+        {
+            image: kaiden,
+            name: 'Kaiden Ko',
+            role: 'Social Events Lead',
+            description: '',
+        },
+        {
+            image: suna,
+            name: 'Sun A Choe',
+            role: 'Project Development Lead',
             description: '',
         },
         // {
         //     image: kate,
         //     name: 'Kate Shin',
-        //     role: 'Design Chair',
+        //     role: 'Design Lead',
         //     description: '',
         // },
-        {
-            image: donggun,
-            name: 'Donggun Lee',
-            role: 'Proj Dev Lead',
-            description: '',
-        },
+        // {
+        //     image: erica,
+        //     name: 'Erica Shin',
+        //     role: 'PR/Marketing Lead',
+        //     description: '',
+        // },
         // {
         //     image: parker,
         //     name: 'Parker Chang',
+        //     role: 'Professional Events',
+        //     description: '',
+        // },
+        // {
+        //     image: kyungtae,
+        //     name: 'Kyungtae Kim',
         //     role: 'Events',
         //     description: '',
         // },
-        {
-            image: junsoo,
-            name: 'Junsoo Kim',
-            role: 'Events',
-            description: 'CECS',
-        },
-        {
-            image: jacob,
-            name: 'Jacob Cho',
-            role: 'Finance',
-            description: 'ye',
-        },
         // {
         //     image: ryan,
         //     name: 'Ryan Jang',
@@ -96,59 +102,27 @@ import kaiden from '../assets/team/kaiden.JPG';
         {
             image: charlotte,
             name: 'Charlotte Kwon',
-            role: 'Events',
+            role: 'Social Events',
             description: '',
         },
-        // {
-        //     image: kyungtae,
-        //     name: 'Kyungtae Kim',
-        //     role: 'Events',
-        //     description: '',
-        // },
-        {
-            image: suna,
-            name: 'Sun-A Choe',
-            role: 'Proj Dev',
-            description: '',
-        },
-        {
-            image: hoyeol,
-            name: 'Hoyeol Baek',
-            role: 'Proj Dev',
-            description: '',
-        },
-        {
-            image: juyeop,
-            name: 'Juyeop Lee',
-            role: 'PR/Marketing',
-            description: 'Freshman',
-        },
-        // {
-        //     image: erica,
-        //     name: 'Erica Shin',
-        //     role: 'PR/Marketing',
-        //     description: '',
-        // },
         // {
         //     image: phillip,
         //     name: 'Phillip Suh',
         //     role: 'PR/Marketing',
         //     description: '',
         // },
-        {
-            image: kaiden,
-            name: 'Kaiden Ko',
-            role: 'Freshmen Rep',
-            description: '',
-        },
+        // {
+        //     image: lauren,
+        //     name: 'Lauren Kim',
+        //     role: 'Social Events',
+        //     description: '',
+        // },
         // {
         //     image: andrew,
         //     name: 'Andrew Park',
         //     role: 'Networking',
         //     description: '',
         // },
-
-
 
     ];
 
