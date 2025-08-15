d248910c0a7cd3cbd366abb29fea2f892fa87f6b
Sun A Choe <sunachoe@usc.edu>
2025-08-15 10:13:26 +0900

    update

--- src/components/Events.js
+++ src/components/Events.js
@@ -6,6 +6,10 @@ import event5 from '../assets/events/event5.jpg'
 import event6 from '../assets/events/event6.jpg'
 import event7 from '../assets/events/event7.jpg'
 import event8 from '../assets/events/event8.jpeg'
+import event9 from '../assets/events/event9.jpg'
+import event10 from '../assets/events/event10.png'
+import event11 from '../assets/events/event11.png'
+
 
 
 const Events = [
@@ -18,6 +22,21 @@ const Events = [
         image: event8,
         name: 'KSEA Banquet 2025',
         description: 'Had a fun night at the Banquet and met people from other California KSEA chapters!',
+    },
+    {
+        image: event10,
+        name: 'Ski Trip 2025',
+        description: 'Big Bear Ski Trip!',
+    },
+    {
+        image: event9,
+        name: 'Fall Retreat 2024',
+        description: 'Retreat at Garden Grove!',
+    },
+     {
+        image: event11,
+        name: 'KATALYST 2024',
+        description: 'First KSEA Katalyst Conference at Atlanta, Georgia',
     },
     {
         image: event1,
